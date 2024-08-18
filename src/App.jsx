8002e5d5b7e25e8a729728eb2afa1370fb16412e
Akrom <akromxolboyev0615@gmail.com>
2024-08-18 23:08:33 +0500

    stert

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Antd } from './antd'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Antd/>
+    </>
+  )
+}
+
+export default App
---
+++ src/antd.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Antd = () => {
+  return (
+    <div>Antd</div>
+  )
+}
